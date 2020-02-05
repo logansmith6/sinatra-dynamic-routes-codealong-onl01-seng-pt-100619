7c13ea6ba308698692e3b2dfcc3422cc1f2eae6d
Logan Smith <logsmith06@gmail.com>
2020-02-05 23:46:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,4 +19,11 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
+  get "/multiply/:num1/:num2" do 
+    @x = params[:num1].to_i 
+    @y = params[:num2].to_1
+    z = @x * @y 
+    "#{z}"
+  end 
+
 end
